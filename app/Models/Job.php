dd0256404f39979066fbf94c56575188f5578ba5
clarkeinon <clarkeinon23@gmail.com>
2025-10-27 16:31:20 +0800

    Chapter 4: Implement Eloquent relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,7 +5,15 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
 use HasFactory;
-// By convention, Laravel assumes a 'jobs' table.
-// We need to tell it to use our 'job_listings' table instead.
 protected $table = 'job_listings';
+
+public function employer()
+{
+return $this->belongsTo(\App\Models\Employer::class);
+}
+public function tags()
+{
+return $this->belongsToMany(\App\Models\Tag::class, foreignPivotKey:
+"job_listing_id");
+}
 }
